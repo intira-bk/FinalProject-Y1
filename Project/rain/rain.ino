9c9b68f632427b81e46b71c3b5fbf402f37bfe4a
intira <bluewhalechanal@gmail.com>
2020-06-10 17:10:13 +0700

    rain

---
+++ Project/rain/rain.ino
@@ -0,0 +1,34 @@
+#include "LiquidCrystal_I2C.h"
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() 
+{
+    Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("rain?");
+  
+  lcd.setCursor(0, 1);
+  lcd.print("Hi");
+
+  delay(2000);
+}
+void loop() 
+{
+  lcd.setCursor(0, 0);
+  lcd.print("rain?");
+ int sensorValue = analogRead(A1);
+ if(sensorValue>=500)
+ {
+  lcd.setCursor(0, 1);
+  lcd.print("No");
+ }
+else
+{
+    lcd.setCursor(0, 1);
+    lcd.print("Yes");
+}
+}
