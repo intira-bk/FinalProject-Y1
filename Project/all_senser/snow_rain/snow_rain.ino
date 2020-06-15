28b3f341b284f3e30b771db25b6c54711a8a9a7e
intira <bluewhalechanal@gmail.com>
2020-06-15 22:48:17 +0700

    snow_rain

--- Project/all_senser/snow_rain/snow_rain.ino
+++ Project/all_senser/snow_rain/snow_rain.ino
@@ -1,10 +1,14 @@
 #define sensorPin A0
-void setup() 
-{
-    Serial.begin(9600);
-    pinMode(sensorPin, INPUT);
+void setup()
+ {
+  Serial.begin(9600);
 }
-void loop() 
-{
-     int sensorValue = analogRead(A0);
+
+
+void loop()
+ {
+  int sensorValue = analogRead(A0);
+  
+  Serial.println(sensorValue);
+  delay(100);
 }
