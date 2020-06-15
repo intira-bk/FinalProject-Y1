6b9fc1f3d4fea4ecdc0483a9c56ad41828880c3a
intira <bluewhalechanal@gmail.com>
2020-06-15 21:11:52 +0700

    LCD

---
+++ Project/all_senser/LCD/LCD.ino
@@ -0,0 +1,21 @@
+#include "LiquidCrystal_I2C.h"
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+void setup() 
+{
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("rain?");
+  
+  lcd.setCursor(0, 1);
+  lcd.print("Hi");
+
+  delay(2000);
+}
+void loop() 
+{
+  lcd.setCursor(0, 0);
+  lcd.print("rain?");
+}
